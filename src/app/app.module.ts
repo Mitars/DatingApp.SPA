798106bd5ee572342cf2140ce9149160f461a5a0
Mitars <mitarkun@gmail.com>
2019-12-25 21:02:48 +0100

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { ValueComponent } from './value/value.component';
+
+@NgModule({
+   declarations: [
+      AppComponent,
+      ValueComponent
+   ],
+   imports: [
+      BrowserModule,
+      HttpClientModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
+})
+export class AppModule { }
